ec63277d7df87476d04be5cad0d21d987b68f702
SwastikaDey62 <deyswastika2020@gmail.com>
2026-07-17 12:39:09 +0530

    Create StringPattern.java
    program to display the pattern:
    A B C D E
    B C D E
    C D E
    D E
    E

---
+++ StringPattern.java
@@ -0,0 +1,14 @@
+public class StringPattern
+{
+    public static void main(String args[]) {
+        String word = "ABCDE";
+        int len = word.length();
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                char ch = word.charAt(j);
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+}
